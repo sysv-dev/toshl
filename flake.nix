54578f8c3912bd7b0d44856617389749c9a14a36
Simon Žlender <pub.git@zlender.si>
2021-12-31 17:29:43 +0100

    Add flake package

--- flake.nix
+++ flake.nix
@@ -5,14 +5,22 @@
 
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            (ruby_3_0.withPackages (ps: with ps; [ faraday ]))
-          ];
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        buildInputs = with pkgs; [ (ruby_3_0.withPackages (ps: with ps; [ faraday ])) ];
+        toshl = pkgs.stdenv.mkDerivation {
+          name = "toshl";
+          src = self;
+          buildInputs = buildInputs;
+          installPhase = "mkdir -p $out/bin; install -t $out/bin toshl";
         };
+      in
+      rec {
+        packages = flake-utils.lib.flattenTree { toshl = toshl; };
+        defaultPackage = packages.toshl;
+        apps.toshl = flake-utils.lib.mkApp { drv = packages.toshl; };
+        defaultApp = apps.toshl;
+        devShell = pkgs.mkShell { buildInputs = buildInputs; };
       }
     );
 }
