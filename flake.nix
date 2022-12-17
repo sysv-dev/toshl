0b535d6ea91e0c87034aeff74e5fdd9860e3fe06
Simon Žlender <pub.git@zlender.si>
2022-12-17 20:08:56 +0100

    update deps

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
   description = "toshl";
 
-  inputs.flake-utils = { url = "github:numtide/flake-utils"; };
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
 
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
